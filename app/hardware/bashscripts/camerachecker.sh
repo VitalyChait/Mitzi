0c7f540ddd3fb46ef7172a84526ea625710d0698
VitalyChait <vitalyhait94@gmail.com>
2022-09-29 14:28:10 +0800

    Delete app directory

--- app/hardware/bashscripts/camerachecker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-v4l2-ctl --list-devices | grep -A1 'Camera' | awk NR==2{print} | { read; echo "${REPLY#${REPLY%?}}";}
--- app/server/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-auth = Blueprint('auth', __name__)
-main = Blueprint('main', __name__)
-
-from . import main_routes, auth_routes, events
